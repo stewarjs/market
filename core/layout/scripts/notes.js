c59342b0be0c8b7280a8c59ac694d24013099a59
Jeff <stewarjs@gmail.com>
2018-03-29 10:49:19 -0500

    Add files via upload

--- core/layout/scripts/notes.js
+++ core/layout/scripts/notes.js
@@ -4,3 +4,32 @@ $( "#add_note" ).on('click', function() {
 	$.modal({title: 'Add Note', parentID: def, content: '<form id="note_form" action="index.php" method="post"><label for="title">Title</label><input type="text" name="title" id="title" required="true" /><label for="note">Note</label><textarea id="note" name="note" required="true"></textarea></form>', footer: '<div class="button-group--directional align--right"><button type="reset" class="button button--gray modal__close">Close</button><button form="note_form" class="button button--blue">Add Note</button></div>'});
 
 });
+
+$('#note_search').on('submit', function(event) {
+	$('.highlight').removeClass('highlight');
+	searchThis(document.getElementById('search').value);
+	event.preventDefault();
+});
+
+function searchThis(query) {
+	var regex = new RegExp(query, "i");
+	  $('.note').each(function() {
+		if (($(this).children('.note__header').text().search(regex) !== -1)) {
+			$(this).children('.note__header').html(selectMatch($(this).children('.note__header').text(), query));
+			($(this).hasClass('no_match') ? $(this).removeClass('no_match') : false);
+		}else if(($(this).children('.note__content').text().search(regex) !== -1)) {
+			$(this).children('.note__content').html(selectMatch($(this).children('.note__content').text(), query));
+			($(this).hasClass('no_match') ? $(this).removeClass('no_match') : false);
+		}else{
+			$(this).addClass('no_match');
+		}
+	  });
+}
+
+function selectMatch(haystack, needle) {
+    //return haystack.replace(new RegExp('(^|)(' + needle + ')(|$)','ig'), '$1<b>$2</b>$3');
+	var reg = new RegExp(needle, 'gi'),
+    		occurrance = haystack.search(reg),
+    		selection = haystack.substr(occurrance - 100, 100);
+    return selection.replace(new RegExp('(^|)(' + needle + ')(|$)','ig'), '$1<span class="highlight">$2</span>$3');
+}
